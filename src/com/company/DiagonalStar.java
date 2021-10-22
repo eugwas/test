b91be6d02dbd86e7b5e113df450dd12b6d51d7a6
Eugeniusz W <eugwas@gmail.com>
2021-10-22 17:16:11 +0200

    init commit

---
+++ src/com/company/DiagonalStar.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class DiagonalStar {
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            printFirstOrLastRow(number);
+            printMiddleRows(number);
+            printFirstOrLastRow(number);
+        }
+    }
+
+    public static void printFirstOrLastRow(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void printMiddleRows(int number) {
+        for (int i = 1; i < number - 1; i++) {
+            System.out.print("*");
+            for (int j = 1; j < number - 1; j++) {
+                if (j == i) {
+                    System.out.print("*");
+                } else if (j == number - 1 - i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("*");
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        DiagonalStar.printSquareStar(8);
+    }
+}
